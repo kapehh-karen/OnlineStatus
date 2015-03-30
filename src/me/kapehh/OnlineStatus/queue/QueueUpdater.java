cc6994911487f45afb9ef10625405957b62ccc4d
kapehh <git@kapehh.net>
2015-03-30 20:57:44 +0400

    add sync

--- src/me/kapehh/OnlineStatus/queue/QueueUpdater.java
+++ src/me/kapehh/OnlineStatus/queue/QueueUpdater.java
@@ -11,6 +11,7 @@ import java.util.List;
  * Created by Karen on 30.03.2015.
  */
 public class QueueUpdater extends BukkitRunnable {
+    private final Object syncLock = new Object();
     private PluginDatabase dbHelper;
     private List<QueueItem> queueItems = new ArrayList<QueueItem>();
 
@@ -19,7 +20,9 @@ public class QueueUpdater extends BukkitRunnable {
     }
 
     public void addInQueue(String worldName, String playerName, int type) {
-        queueItems.add(new QueueItem(playerName, worldName, type));
+        synchronized (syncLock) {
+            queueItems.add(new QueueItem(playerName, worldName, type));
+        }
     }
 
     public void forceRun() {
@@ -28,31 +31,33 @@ public class QueueUpdater extends BukkitRunnable {
 
     @Override
     public void run() {
-        for (QueueItem item : queueItems) {
-            switch (item.getTypeOfRequest()) {
-                case QueueItem.JOIN:
-                    try {
-                        dbHelper.prepareQueryUpdate("INSERT INTO players(name,online) VALUES (?,1) ON DUPLICATE KEY UPDATE online = 1", item.getPlayerName());
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case QueueItem.LEAVE:
-                    try {
-                        dbHelper.prepareQueryUpdate("UPDATE players SET online = 0 WHERE name = ?", item.getPlayerName());
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case QueueItem.NEW_WORLD:
-                    try {
-                        dbHelper.prepareQueryUpdate("UPDATE players SET lastworld = ? WHERE name = ?", item.getWorldName(), item.getPlayerName());
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
+        synchronized (syncLock) {
+            for (QueueItem item : queueItems) {
+                switch (item.getTypeOfRequest()) {
+                    case QueueItem.JOIN:
+                        try {
+                            dbHelper.prepareQueryUpdate("INSERT INTO players(name,online) VALUES (?,1) ON DUPLICATE KEY UPDATE online = 1", item.getPlayerName());
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case QueueItem.LEAVE:
+                        try {
+                            dbHelper.prepareQueryUpdate("UPDATE players SET online = 0 WHERE name = ?", item.getPlayerName());
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case QueueItem.NEW_WORLD:
+                        try {
+                            dbHelper.prepareQueryUpdate("UPDATE players SET lastworld = ? WHERE name = ?", item.getWorldName(), item.getPlayerName());
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
             }
+            queueItems.clear();
         }
-        queueItems.clear();
     }
 }
